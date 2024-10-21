62f3b27ed7dcade08d55653feb72662c507d6cd3
Atharv Rastogi <atharvrastogi714@gmail.com>
2024-10-21 17:36:41 +0530

    Update orgmain.jsx

--- src/components/organising/orgmain.jsx
+++ src/components/organising/orgmain.jsx
@@ -18,7 +18,8 @@ export function TabsDemo() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-[90rem] mx-auto w-full  items-start justify-start my-40 font-spacemono">
+
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-[90rem] mx-auto w-full  items-start justify-start mb-40 font-spacemono">
       <Tabs tabs={tabs} />
     </div>
   );
