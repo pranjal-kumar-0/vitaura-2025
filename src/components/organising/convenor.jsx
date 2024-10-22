8dcae0f9762e982a23ce5be841f472ae34484c53
ecellvitap <devvratpathak2019@gmail.com>
2024-10-22 11:26:13 +0530

    Update convenor.jsx

--- src/components/organising/convenor.jsx
+++ src/components/organising/convenor.jsx
@@ -44,7 +44,7 @@ const Convenor = () => {
               Abhinav Pentani
             </h1>
             <h2 className="font-spacemono">
-              Convener VITAURA'24 <br /> Club Advisor - ECell
+              Convener VITAURA'24 <br /> Club Advisor - Entreprenership Club
             </h2>
             <p
               className="desc"
