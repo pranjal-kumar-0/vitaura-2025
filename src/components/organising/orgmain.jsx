e6f0c3a8ad6d37af0d58822beaf2807f15815cfd
ecellvitap <devvratpathak2019@gmail.com>
2024-10-23 02:08:44 +0530

    size

--- src/components/organising/orgmain.jsx
+++ src/components/organising/orgmain.jsx
@@ -32,7 +32,7 @@ export function TabsDemo() {
   return (
     <div className="px-5 lg:px-0 min-h-screen md:min-h-[0] relative flex flex-col max-w-[90rem] mx-auto w-full items-center justify-center font-spacemono">
       <div>
-        <h1 className="bg-gradient-to-r from-[#F50062] to-[#b700ba] inline-block text-transparent bg-clip-text text-6xl md:text-8xl font-fluxbox mb-6">
+        <h1 className="bg-gradient-to-r from-[#F50062] to-[#b700ba] inline-block text-transparent bg-clip-text text-6xl md:text-7xl font-fluxbox mb-8">
           Organising Committee
         </h1>
       </div>
